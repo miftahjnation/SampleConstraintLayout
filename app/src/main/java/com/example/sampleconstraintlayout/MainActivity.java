fa33ee5c7e2815a1341761a57475500e8ab40318
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-05 04:13:09 +0700

    Add code deklarasi btn, edittext dan menghubungkannya ke layout yang ada

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -9,10 +9,28 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Deklarasi Variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variable untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variable untuk menyimpan email dan password
+    String nama, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variable btnLogin dengan component button pada layout
+        btnLogin=findViewById(R.id.btSignIn);
+
+        //Menghubungkan variable edemail dengan component edittext email pada layout
+        edemail=findViewById(R.id.edEmail);
+
+        //Menghubungkan variable edpassword dengan component edittext password pada layout
+        edpassword=findViewById(R.id.edPassword);
     }
 }
