dadd159471f1c23416829aba0a9b64836dd6ee86
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-05 04:06:58 +0700

    Change id pada EditText dan button serta menambahkan import android widget button,edit text dan toast ke MainActivity.java

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sampleconstraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
