3e1db3635ece5f1579be56051a54a21d1fa6d0c2
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-28 11:30:33 +0700

    Menambahkan layout dan class activity home serta menambahkan text view dan list view pada layout activity_home.xml

---
+++ app/src/main/java/com/example/sampleconstraintlayout/Home_Activity.java
@@ -0,0 +1,16 @@
+package com.example.sampleconstraintlayout;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home_Activity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
